0e8cf27cedc6338064c5a3c973bd2a4e6a88af03
vincentvbh <b05902122@ntu.edu.tw>
2021-09-14 13:34:51 +0800

    add soft links to libopencm3

--- setup_libopencm3.sh
+++ setup_libopencm3.sh
@@ -1,11 +1,7 @@
 
 get_libopencm3 (){
 
-git clone https://github.com/libopencm3/libopencm3
-cd libopencm3
-git checkout 2483e2e358f1a0d6526c63aa0b5ef2dd3358039e
-make -j4
-cd ../
+ln -s ../../../libopencm3/
 
 }
 
