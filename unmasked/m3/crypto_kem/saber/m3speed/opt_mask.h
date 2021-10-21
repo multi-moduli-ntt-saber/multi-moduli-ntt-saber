36172b6ae19ba3f3200342cf0c535febf18abb02
vincentvbh <vincentvbh7@gmail.com>
2021-10-21 22:19:45 +0800

    soft links

--- unmasked/m3/crypto_kem/saber/m3speed/opt_mask.h
+++ unmasked/m3/crypto_kem/saber/m3speed/opt_mask.h
@@ -2,11 +2,11 @@
 #define OPT_MASK
 
 #if SABER_L == 2
-    #define MASK 0x8884
+    #define MASK 0x1111
 #elif SABER_L == 3
-    #define MASK 0x8882
+    #define MASK 0x1111
 #elif SABER_L == 4
-    #define MASK 0x8882
+    #define MASK 0x1111
 #endif
 
 #endif
