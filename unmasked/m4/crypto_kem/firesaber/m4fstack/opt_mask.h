7660e3d5d33fa84bfc1c7e771400015fa7a60f85
vincentvbh <vincentvbh7@gmail.com>
2022-01-18 23:01:39 +0800

    restore soft links

--- unmasked/m4/crypto_kem/firesaber/m4fstack/opt_mask.h
+++ unmasked/m4/crypto_kem/firesaber/m4fstack/opt_mask.h
@@ -1,14 +1,1 @@
-#ifndef OPT_MASK_H
-#define OPT_MASK_H
-
-#if SABER_L == 2
-    #define MASK 0x8882
-#elif SABER_L == 3
-    #define MASK 0x8882
-#elif SABER_L == 4
-    #define MASK 0x8882
-#endif
-
-#endif
-
-
+../../saber/m4fstack/opt_mask.h
--- unmasked/m4/crypto_kem/lightsaber/m4fspeed/opt_mask.h
+++ unmasked/m4/crypto_kem/lightsaber/m4fspeed/opt_mask.h
@@ -1,14 +1,1 @@
-#ifndef OPT_MASK_H
-#define OPT_MASK_H
-
-#if SABER_L == 2
-    #define MASK 0x1111
-#elif SABER_L == 3
-    #define MASK 0x1111
-#elif SABER_L == 4
-    #define MASK 0x1111
-#endif
-
-#endif
-
-
+../../saber/m4fspeed/opt_mask.h
